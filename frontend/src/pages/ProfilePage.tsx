2c5bb8e58fc3e69bd969e65786fba3aac4155215
Johannes Nowack <johannesnowack@gebetshaus.org>
2025-12-11 15:49:17 +0100

    tel numer

--- frontend/src/pages/ProfilePage.tsx
+++ frontend/src/pages/ProfilePage.tsx
@@ -1,7 +1,32 @@
+import { Form } from "react-router";
 import { Button } from "../components/Button";
 import { TextBox } from "../components/TextBox";
+import { useState, type FormEvent } from "react";
+
+const getValuesFromEvent = (ev: FormEvent<HTMLFormElement>) => {
+  const formData = new FormData(ev.currentTarget);
+  const values = Object.fromEntries(formData);
+  return values;
+};
+
+const validate = (number: string) => {
+  console.log(number);
+  if (!number) {
+    return "bitte nummer angeben";
+  } else {
+    if (number.length < 9) {
+      return "zu wenig ziffern";
+    }
+  }
+  return null;
+};
 
 export const ProfilePage = () => {
+  const [number, setNumber] = useState("");
+  // const [errorMessage, setErrorMessage] = useState<string | null>("");
+  // setErrorMessage(validate(number));
+  const errorMessage = validate(number);
+
   return (
     <div className="bg-pure mx-auto w-full max-w-md space-y-8 p-8">
       <div>
@@ -13,9 +38,18 @@ export const ProfilePage = () => {
         description="Lade ein Foto hoch"
         type="file"
       />
-      <TextBox label="Dein eigener Code" description="Lade ein Foto hoch" />
+      <Form onSubmit={(ev) => console.log(getValuesFromEvent(ev))}>
+        <TextBox
+          name="code"
+          label="Deine Handynummer"
+          description="bitte 9 stellig"
+          value={number}
+          onChange={(ev) => setNumber(ev.target.value)}
+        />
+        {errorMessage}
+      </Form>
       <TextBox
-        label="Deine Handynummer"
+        label="Deine Code"
         description="Lade ein Foto hoch"
         type="file"
       />
